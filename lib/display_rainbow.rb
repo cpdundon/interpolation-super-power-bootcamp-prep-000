cce9f319c1e59bc914dd01c17831caf3cf955f90
Chris Dundon <chris.dundon@gmail.com>
2018-03-16 22:53:26 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -5,5 +5,4 @@ def display_rainbow(arr)
   out = "#{first[0]}: #{arr[0]}, #{first[1]}: #{arr[1]}, #{first[2]}: #{arr[2]}, #{first[3]}: #{arr[3]}, #{first[4]}: #{arr[4]}, #{first[5]}: #{arr[5]}, #{first[6]}: #{arr[6]}"
   
   puts(out)
-  out
 end  
